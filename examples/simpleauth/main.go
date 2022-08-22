8d67681111854f4a63495fe6290f4c8e1daed2e6
alanfrye <alan_frye@selinc.com>
2022-08-22 15:58:44 -0400

    initial commit for emissary auth demo

---
+++ examples/simpleauth/main.go
@@ -0,0 +1,47 @@
+// Copyright (c) 2021 - 2022 Schweitzer Engineering Laboratories, Inc.
+// SEL Confidential
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	http.HandleFunc("/bfauth/demoapp/status/", authenticate)
+	err := http.ListenAndServe(":3333", nil)
+	if err != nil {
+		fmt.Printf("Error starting server: %v", err)
+		os.Exit(1)
+
+	}
+	fmt.Printf("Serving running on port 3333")
+}
+
+func authenticate(w http.ResponseWriter, r *http.Request) {
+	// UNIX Time is faster and smaller than most timestamps
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Info().Msg("authenticate handler fired")
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "BLUEFRAME_SESSION",
+		Value:    uuid.New().String(),
+		MaxAge:   86400,
+		HttpOnly: true,
+		Secure:   false,
+		SameSite: http.SameSiteStrictMode,
+		Expires:  time.Now().Add(time.Hour),
+	})
+	// w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+	// w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("x-myheader", "simplauth")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Login succeed"))
+}
